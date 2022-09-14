3efb1d075ce59d2543c63362039d9a82ca8e0361
mariiaglushak <glushakmash@gmail.com>
2022-09-14 18:55:32 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -12,53 +12,55 @@ const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
 const onInput = event => {
-  if (event.target.value === '') {
+  if (event.target.value === '' || event.target.value.length < 2) {
     countryList.innerHTML = '';
     countryInfo.innerHTML = '';
-  }
-  countryList.innerHTML = '';
-  countryInfo.innerHTML = '';
+    return;
+  } else {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
 
-  const nameCountry = event.target.value;
+    const nameCountry = event.target.value;
 
-  fetchCountries(nameCountry.trim())
-    .then(data => {
-      const htmlString = data
-        .map(
-          obj => `<li class="country-item">
+    fetchCountries(nameCountry.trim())
+      .then(data => {
+        const htmlString = data
+          .map(
+            obj => `<li class="country-item">
         <img src="${obj.flags.svg}" class ="flags-country">
         <p class="country-name">${obj.name.official}</p>
       </li>`
-        )
-        .join('');
-      if (nameCountry.length >= 2) {
-        countryList.innerHTML = htmlString;
-      }
+          )
+          .join('');
+        if (nameCountry.length >= 2) {
+          countryList.innerHTML = htmlString;
+        }
 
-      if (data.length === 1) {
-        countryInfo.innerHTML = `<p class="text-info"><span class ="info-title">Capital:</span>${
-          data[0].capital
-        }</p>
+        if (data.length === 1) {
+          countryInfo.innerHTML = `<p class="text-info"><span class ="info-title">Capital:</span>${
+            data[0].capital
+          }</p>
         <p class="text-info"><span class ="info-title">Population:</span>${
           data[0].population
         }</p>
         <p class="text-info"><span class ="info-title">Languages:</span>${Object.values(
           data[0].languages
         )}</p>`;
-      }
+        }
 
-      if (data.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+        if (data.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
 
-  console.log(nameCountry);
+    console.log(nameCountry);
+  }
 };
 
 input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
