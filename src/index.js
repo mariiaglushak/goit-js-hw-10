cce0e7c6ace1d299015bcaee1e0bf794a17eeca6
mariiaglushak <glushakmash@gmail.com>
2022-09-14 19:48:23 +0300

    поправка

--- src/index.js
+++ src/index.js
@@ -24,31 +24,31 @@ const onInput = event => {
 
     fetchCountries(nameCountry.trim())
       .then(data => {
-        const htmlString = data
-          .map(
-            obj => `<li class="country-item">
+        if (data.length <= 10) {
+          const htmlString = data
+            .map(
+              obj => `<li class="country-item">
         <img src="${obj.flags.svg}" class ="flags-country">
         <p class="country-name">${obj.name.official}</p>
       </li>`
-          )
-          .join('');
-        if (nameCountry.length >= 2) {
-          countryList.innerHTML = htmlString;
-        }
+            )
+            .join('');
+          if (nameCountry.length >= 2) {
+            countryList.innerHTML = htmlString;
+          }
 
-        if (data.length === 1) {
-          countryInfo.innerHTML = `<p class="text-info"><span class ="info-title">Capital:</span>${
-            data[0].capital
-          }</p>
+          if (data.length === 1) {
+            countryInfo.innerHTML = `<p class="text-info"><span class ="info-title">Capital:</span>${
+              data[0].capital
+            }</p>
         <p class="text-info"><span class ="info-title">Population:</span>${
           data[0].population
         }</p>
         <p class="text-info"><span class ="info-title">Languages:</span>${Object.values(
           data[0].languages
         )}</p>`;
-        }
-
-        if (data.length > 10) {
+          }
+        } else {
           Notiflix.Notify.info(
             'Too many matches found. Please enter a more specific name.'
           );
